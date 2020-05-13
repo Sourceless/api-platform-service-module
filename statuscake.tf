73b9c0991c6f6ce495f417961c17a0174135ed36
jalal <jalalhosseiny@gmail.com>
2020-05-13 15:52:12 +0100

    AMB-52 add status and ping urls to statuscake

--- statuscake.tf
+++ statuscake.tf
@@ -3,10 +3,24 @@ provider "statuscake" {
   apikey = var.status_cake_api_key
 }
 
-resource "statuscake_test" "google" {
-  website_name = "google.com"
-  website_url  = "www.google.com"
+locals {
+  website_name = "${var.apigee_environment}.api.service.nhs.uk"
+  ping_url = "${local.website_name}/${var.path}/_ping"
+  status_url = "${local.website_name}/${var.path}/_status"
+}
+
+resource "statuscake_test" "ping" {
+  website_name = local.website_name
+  website_url  = local.ping_url
   test_type    = "HTTP"
   check_rate   = 300
-  contact_group   = var.status_cake_contact_group
+  contact_group   = [var.status_cake_contact_group]
+}
+
+resource "statuscake_test" "status" {
+  website_name = local.website_name
+  website_url  = local.status_url
+  test_type    = "HTTP"
+  check_rate   = 300
+  contact_group   = [var.status_cake_contact_group]
 }
